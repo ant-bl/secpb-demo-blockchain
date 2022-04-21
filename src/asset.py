dc48377852d1d6b0ec28079d983c513997bc4643
antoine <antoine.blin@gandi.net>
2022-04-21 10:46:32 +0200

    Format code

--- src/asset.py
+++ src/asset.py
@@ -1,7 +1,3 @@
-import json
-import hashlib
-import binascii
-
 class AssetCreate():
     """Class for asset creation"""
 
@@ -9,18 +5,17 @@ class AssetCreate():
         self.name = ""
         self.open = True
         self.address = ""
-        self.qty =1
+        self.qty = 1
         self.minAmount = 0.01
 
-
-    def asset_creation(self,access):
-        asset_params={"name":self.name,"open":self.open}
+    def asset_creation(self, access):
+        asset_params = {"name": self.name, "open": self.open}
         try:
-            transactionID=access.issue(self.address,asset_params,self.qty,self.minAmount)
+            transactionID = access.issue(self.address, asset_params, self.qty, self.minAmount)
             print("Transaction Id", transactionID)
             return transactionID
 
-        except :
+        except:
             print("Asset already exist !")
 
     def set_asset_params(self, target):
@@ -30,10 +25,9 @@ class AssetCreate():
         self.qty = target[3]
         self.minAmount = 0.01
 
-    
-    def sendWithData(self,address,access,data):
+    def sendWithData(self, address, access, data):
 
         print("Data hex is: ", data)
-        transactionID=access.sendwithdata(address,{self.name : self.qty},data)
+        transactionID = access.sendwithdata(address, {self.name: self.qty}, data)
 
         return transactionID
