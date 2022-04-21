106c7320e58ae4015f974742f2f70f9a8bf6b3fe
antoine <antoine.blin@gandi.net>
2022-04-21 14:35:11 +0200

    clean code

--- src/asset.py
+++ src/asset.py
@@ -1,33 +1,32 @@
-class AssetCreate():
-    """Class for asset creation"""
+import logging
+
+
+class AssetCreate:
 
     def __init__(self):
         self.name = ""
         self.open = True
         self.address = ""
         self.qty = 1
-        self.minAmount = 0.01
+        self.min_amount = 0.01
 
     def asset_creation(self, access):
         asset_params = {"name": self.name, "open": self.open}
         try:
-            transactionID = access.issue(self.address, asset_params, self.qty, self.minAmount)
-            print("Transaction Id", transactionID)
-            return transactionID
-
-        except:
-            print("Asset already exist !")
+            transaction_id = access.issue(self.address, asset_params, self.qty, self.min_amount)
+            logging.info(f"Transaction Id: {transaction_id}")
+            return transaction_id
+        except Exception:
+            logging.error(f"asset already exists")
 
     def set_asset_params(self, target):
         self.name = target[0]
         self.open = target[1]
         self.address = target[2]
         self.qty = target[3]
-        self.minAmount = 0.01
+        self.min_amount = 0.01
 
-    def sendWithData(self, address, access, data):
+    def send_with_data(self, address, access, data):
+        transaction_id = access.sendwithdata(address, {self.name: self.qty}, data)
 
-        print("Data hex is: ", data)
-        transactionID = access.sendwithdata(address, {self.name: self.qty}, data)
-
-        return transactionID
+        return transaction_id
